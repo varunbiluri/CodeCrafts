9ac4e11f3ef6eee94679ea910afb484d0f8de60d
Saikumar guduru <skg.13.edu@gmail.com>
2023-09-14 10:00:25 +0530

    update

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var codesRouter = require('./routes/codes');
+var editorRouter = require('./routes/editor');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/codes', codesRouter);
+app.use('/editor', editorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
